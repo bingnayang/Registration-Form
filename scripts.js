e982eba51fff24cf31bb5ffd69311206af7a4ed6
BingNa Yang <bingnayang@gmail.com>
2020-02-15 19:49:27 -0500

    Check if password and confirm password are same

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 function submitNow(){
     var checkBox = document.getElementById("policyCheck");
-    
-    // Check if User check accept the policy
+    var passwordOne = document.forms["form"]["password"].value;
+    var passwordTwo = document.forms["form"]["confirmPassword"].value;
+
+    // Check if user input the match password
+    if(passwordOne !== passwordTwo){
+        alert('Please check your password if it\'s match');
+        return false;
+    }
+    // Check if user check accept the policy
     if(checkBox.checked == false){
         alert('Please accept the term of use & privacy policy');
         return false;
